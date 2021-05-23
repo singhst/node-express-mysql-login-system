b0bf8c98443468dbaa742bf2d2402ae54e619140
singhst <singhst@users.noreply.github.com>
2021-05-23 20:05:25 +0800

    test node.js to mysql connection

---
+++ test-node-mysql-connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql')
+
+console.log("import lib");
+
+let connection = mysql.createConnection({
+    host: 'localhost', // host for connection
+    // port: 3306, // default port for mysql is 3306
+    database: 'test', // database from which we want to connect out node application
+    user: 'root', // username of the mysql connection
+    password: '00000000' // password of the mysql connection
+});
+
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+        console.log("error occured while connecting");
+    }
+    else {
+        console.log("connection created with Mysql successfully");
+    }
+});

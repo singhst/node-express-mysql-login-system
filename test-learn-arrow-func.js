035bd16ad55880a93fc5f1377c9af82ee2b85494
singhst <singhst@users.noreply.github.com>
2021-05-29 14:25:29 +0800

    learning only

---
+++ test-learn-arrow-func.js
@@ -0,0 +1,38 @@
+// Arrow function vs Normal function
+// 
+// Arrow function pros:
+// variable becomes a function
+//
+/////////////////////////////////////
+
+
+//1. Normal function
+function logInfo(number) {
+	return number * number;
+}
+input = 3;
+var return_var = logInfo(input)
+console.log(return_var);
+
+
+//2. Arrow function
+input = 4;
+var return_var = (number) => {
+    return number * number
+}
+console.log(return_var(input));
+
+
+//3. Arrow function - no arguments
+var return_var = () => {
+    var number = 5;
+    return number * number
+}
+console.log(return_var());
+
+
+/////////////////////////////////////
+// output:
+// 9
+// 16
+// 25
